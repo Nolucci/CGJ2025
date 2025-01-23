5eeeba898ad29c131233edea8ae57d50062bb7ac
Pilgrimeru <84448802+Pilgrimeru@users.noreply.github.com>
2025-01-23 22:04:59 +0100

    player data first part

---
+++ resources/player_data/player_data.gd
@@ -0,0 +1,6 @@
+extends Resource
+
+class_name PlayerData
+
+@export var name: String = "Alain"
+@export var upgrade: Array[PlayerUpgrade]  = []
---
+++ resources/player_data/upgrade.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name PlayerUpgrade
+
+@export var primary_key: int
+@export var name: String
+@export var descrition: String
+@export var price: int
+@export var currentLevel: int
