d33571139fd07fb420969bd17189656f6819f6d1
Unmoriel <33257963+Unmoriel@users.noreply.github.com>
2025-01-24 17:58:28 +0100

    wave update

--- scripts/global/scene_principale.gd
+++ scripts/global/scene_principale.gd
@@ -2,34 +2,55 @@ extends Node2D
 
 @onready var spawn_point = $spawnPoints  # Le nœud contenant les spawn points
 @onready var go_points = $goPoints  # Le nœud contenant les points cibles
-@onready var baby_scene = preload("res://scenes/enemie/baby.tscn")  # Précharger la scène des babies
 
 @export var waves: Array[Wave] = []
 var currentWave: int = 1
 var currentLevel: int = 1
 
-var enemies: Array[Enemie] = []
+@export var enemies: Array[Enemie] = []
 
 func _ready():
-	spawn_enemies(false)
+	pass
 
-func _process(delta):
+func _process(_delta):
 	if enemies.size() == 0:
-		if waves[currentWave - 1].nbLevelBefortBoss == currentLevel:
+		if(currentWave - 1) == waves.size():
+			currentWave = 1
+			print("c'est repartis");
+		if waves[currentWave - 1].nbLevelBeforeBoss == currentLevel:
 			spawn_enemies(true)
+			print("currentLevel : ", currentLevel)
+			print("currentWave : ", currentWave)
 			currentLevel = 1
 			currentWave += 1
 		else :
 			spawn_enemies(false)
+			print("currentLevel : ", currentLevel)
+			print("currentWave : ", currentWave)
 			currentLevel += 1
-			
+		
+	if Input.is_action_just_released("fart"):
+		if enemies.size() > 0:
+			for enemy in enemies:
+				remove_child(enemy)
+		enemies.clear()
 
 func spawn_enemies(boss: bool):
-	for marker in spawn_point.get_children():
-		var enemy_instance = waves[currentWave - 1].typeEnemy.instantiate()
+	var enemy_scene: PackedScene
+	var nb: int
+	if boss:
+		nb = waves[currentWave - 1].nbBoss
+		enemy_scene = waves[currentWave - 1].typeBoss
+	else:
+		enemy_scene = waves[currentWave - 1].typeEnemy
+		nb = waves[currentWave - 1].nbEnemies
+
+	for i in range(nb):
+		var markers = spawn_point.get_children()
+		var enemy_instance = enemy_scene.instantiate()
 		enemies.append(enemy_instance)
 		add_child(enemy_instance)
-		enemy_instance.position = marker.global_position
+		enemy_instance.position = markers[i % markers.size() - 1].global_position
 
 	var free_point = go_points.get_children()
 	
