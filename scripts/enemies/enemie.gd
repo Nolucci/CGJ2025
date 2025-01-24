b965f0a24f60c8fd53b1862971d5a1c3df34ff22
Unmoriel <33257963+Unmoriel@users.noreply.github.com>
2025-01-24 17:18:45 +0100

    waves

--- scripts/enemies/enemie.gd
+++ scripts/enemies/enemie.gd
@@ -4,6 +4,11 @@ extends CharacterBody2D
 @export var movement: Enemie_movement
 @export var start_movement: bool = false
 var screen_size: Vector2
+var target_position: Vector2 = Vector2.ZERO
+
+func goTo(position: Vector2):
+	target_position = position
+	set_start_movement(false)
 
 
 func set_start_movement(start_movement: bool):
@@ -16,3 +21,17 @@ func _process(delta):
 			movement.process_movement(self, delta, screen_size)
 		else:
 			push_error("Movement script is not assigned!")
+	else:
+		if target_position != Vector2.ZERO:
+			goToTargetPosition()
+
+func goToTargetPosition():
+	var direction = (target_position - position).normalized()
+	velocity = direction * 100.0
+	move_and_slide()
+	
+	# Vérification si la position cible est atteinte
+	if position.distance_to(target_position) < 5.0:  # Tolérance de 5 unités
+		velocity = Vector2.ZERO  # Arrêter le mouvement
+		target_position = Vector2.ZERO
+		set_start_movement(true)
--- scripts/global/scene_principale.gd
+++ scripts/global/scene_principale.gd
@@ -4,23 +4,36 @@ extends Node2D
 @onready var go_points = $goPoints  # Le nœud contenant les points cibles
 @onready var baby_scene = preload("res://scenes/enemie/baby.tscn")  # Précharger la scène des babies
 
-func _ready():
-	var free_points = go_points.get_children()  # Obtenez une copie des enfants de goPoints
-	
-	for i in range(4):
-		if free_points.size() > 0:
-			var point = free_points.pick_random()
-			free_points.erase(point)
-			
-			if point is Marker2D:
-				print("Spawning baby at:", point.global_position)
-				spawn_baby(point.global_position)
+@export var waves: Array[Wave] = []
+var currentWave: int = 1
+var currentLevel: int = 1
 
-func spawn_baby(position: Vector2):
-	if baby_scene:
-		var baby_instance = baby_scene.instantiate()
-		add_child(baby_instance)
-		baby_instance.position = position
-		print("Baby instance:", baby_instance, "spawned at:", position)
-	else:
-		push_error("Baby scene not loaded!")
+var enemies: Array[Enemie] = []
+
+func _ready():
+	spawn_enemies(false)
+
+func _process(delta):
+	if enemies.size() == 0:
+		if waves[currentWave - 1].nbLevelBefortBoss == currentLevel:
+			spawn_enemies(true)
+			currentLevel = 1
+			currentWave += 1
+		else :
+			spawn_enemies(false)
+			currentLevel += 1
+			
+
+func spawn_enemies(boss: bool):
+	for marker in spawn_point.get_children():
+		var enemy_instance = waves[currentWave - 1].typeEnemy.instantiate()
+		enemies.append(enemy_instance)
+		add_child(enemy_instance)
+		enemy_instance.position = marker.global_position
+
+	var free_point = go_points.get_children()
+	
+	for enemy: Enemie in enemies:
+		var marker = free_point.pick_random()
+		free_point.erase(marker)
+		enemy.goTo(marker.global_position)
