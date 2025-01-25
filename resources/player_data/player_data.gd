716e433943d894b7abfbae1c4c69a40fe987f646
BanPerm <peterdesete@gmail.com>
2025-01-25 03:47:15 +0100

    Menu achat cosmetique patch

--- resources/player_data/player_data.gd
+++ resources/player_data/player_data.gd
@@ -8,6 +8,8 @@ class_name PlayerData
 @export var upgrades: Array[PlayerUpgrade] = []
 @export var isInvinsible: bool = false
 
+signal upgrade_bought(upgrade_name: String, new_level: int, remaining_money: int, name_icon: String)
+
 # Fonction pour acheter une amélioration
 func buy_upgrade(primary_key: int) -> bool:
 	var upgrade = find_upgrade_by_key(primary_key)
@@ -19,7 +21,7 @@ func buy_upgrade(primary_key: int) -> bool:
 	money -= upgrade.price
 	upgrade.current_level += 1
 	print("Amélioration achetée:", upgrade.name, "Niveau actuel:", upgrade.current_level)
-	emit_signal("upgrade_bought", upgrade.name, upgrade.current_level, money)
+	emit_signal("upgrade_bought", upgrade.name, upgrade.current_level, money, upgrade.link_icon_buy_menu)
 	return true
 
 # Fonction pour trouver une amélioration par clé primaire
